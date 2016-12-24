8a29df3d3d42320bdd8fbb0dd5b0ed74b3545df0
Konstantin S. <havendv@gmail.com>
2016-12-24 23:22:39 +1000

    Changed Object

--- MakeRuler/Object.cs
+++ MakeRuler/Object.cs
@@ -5,8 +5,14 @@ namespace Example1
     public class Object
     {
         public int Layer { get; set; }
-        public Dictionary<int, Line> Lines { get; set; }
+        public SortedDictionary<int, Line> Lines { get; set; }
         public Point2D Min { get; set; }
         public Point2D Max { get; set; }
+        public int Medium { get; set; }
+
+        public Object(int medium)
+        {
+            Medium = medium;
+        }
     }
 }
