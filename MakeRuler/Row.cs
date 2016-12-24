5c6884d34f4f5da63f1d7e75a0cec4eee8bb2ebb
Konstantin S. <havendv@gmail.com>
2016-12-24 23:58:45 +1000

    Changed Row

--- MakeRuler/Row.cs
+++ MakeRuler/Row.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Example1
 {
     public class Row
     {
-        public int Id { get; set; }
-        public int FirstPixel { get; set; }
-        public List<Area> Areas { get; set; }
+
+        public SortedDictionary<int, int> Data { get; set; }
 
         public Row()
         {
-            Areas = new List<Area>();
+            Data = new SortedDictionary<int, int>();
         }
 
-        public Row(int id, int firstPixel) :
-            this()
+        public void AddLine(Line line)
         {
-            Id = id;
-            FirstPixel = firstPixel;
+            for (var i = line.Start; i <= line.End; ++i)
+            {
+                Data[i] = line.Medium;
+            }
         }
 
-        public Row(string id, string firstPixel) :
-            this(
-                int.Parse(id),
-                int.Parse(firstPixel)
-            )
-        {}
+        public List<Line> ToLines()
+        {
+            var lines = new List<Line>();
+
+            var currentMedium = Data.First().Value;
+            var start = Data.First().Key;
+            for (var i = start;i <= Data.Last().Key; ++i)
+            {
+                if (Data[i] != currentMedium)
+                {
+                    lines.Add(new Line(start, i, currentMedium));
+                    currentMedium = Data[i];
+                    start = i;
+                }
+            }
+            lines.Add(new Line(start, Data.Last().Key, currentMedium));
+
+            return lines;
+        }
     }
 }
