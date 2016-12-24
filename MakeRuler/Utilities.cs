1c1d49ceeda3ac90ca7d4d5fb51e2a2b0a28bddb
Konstantin S. <havendv@gmail.com>
2016-12-24 23:34:30 +1000

    Added Round to Extensions

---
+++ MakeRuler/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeRuler
+{
+    static class Utilities
+    {
+        public static int Round(this double value)
+        {
+            return Convert.ToInt32(value);
+        }
+    }
+}
