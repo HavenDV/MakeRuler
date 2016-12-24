c70457ffaae826c9d37e4602ab75cf7c1742dbcf
Konstantin S. <havendv@gmail.com>
2016-12-24 23:56:10 +1000

    Added Medium to Line

--- MakeRuler/Line.cs
+++ MakeRuler/Line.cs
@@ -1,13 +1,20 @@
-﻿namespace Example1
+﻿using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Example1
 {
     public class Line
     {
         public int Start { get; set; }
         public int End { get; set; }
-        public Line(int start, int end)
+        public int Medium { get; set; }
+
+        public Line(int start, int end, int medium)
         {
             Start = start;
             End = end;
+            Medium = medium;
         }
     }
 }
