df6b69be96cc989cfeb3244a9eb08460157450ad
Konstantin S. <havendv@gmail.com>
2016-12-25 14:52:59 +1000

    Added minX, minY, maxX, maxY for giving rounded value

--- MakeRuler/Point2D.cs
+++ MakeRuler/Point2D.cs
@@ -6,12 +6,23 @@ namespace MakeRuler
     {
         public double X { get; set; }
         public double Y { get; set; }
-        public int iX {
-            get { return X.Round(); }
+
+        public int minX {
+            get { return X.RoundMin(); }
         }
-        public int iY {
-            get { return Y.Round(); }
+
+        public int minY {
+            get { return Y.RoundMin(); }
         }
+
+        public int maxX {
+            get { return X.RoundMax(); }
+        }
+
+        public int maxY {
+            get { return Y.RoundMax(); }
+        }
+
         public Point2D(double x, double y)
         {
             X = x;
