7a9f3e424e90f75a1c55608b8ae43761761c49a6
Konstantin S. <havendv@gmail.com>
2016-12-25 14:52:07 +1000

    Changed data to original

--- MakeRuler/Form1.cs
+++ MakeRuler/Form1.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 using System.Reflection;
 using System.Threading;
 
-namespace Example1
+namespace MakeRuler
 {
     public partial class Form1 : Form
     {
@@ -26,20 +26,26 @@ namespace Example1
             var scene = new Scene();
             var h = height / 300.0;
             var scale = 1 / 0.5;
+            //CORG(1) = 'phantom';
+            //CORG(2) = 'center';
+            //CORG(3) = 'left';
+            //CORG(4) = 'bottom';
+            //CORG(5) = 'right';
+            //CORG(6) = 'top';
             scene.Add(new Rect(scale * (80 + 80 * h), scale * 40 * h, scale * (280 - 80 * h), scale * (160 - 40 * h), 1));
             scene.Add(new Circle(scale * (80 + 80 * h), scale * 80, scale * (80 - 40 * h), 1));
             scene.Add(new Circle(scale * (280 - 80 * h), scale * 80, scale * (80 - 40 * h), 1));
-            scene.Add(new Circle(scale * (180), scale * (20 + 40 * h), scale * 5.5, 2));
-            scene.Add(new Circle(scale * (180), scale * (140 - 40 * h), scale * 5.5, 3));
-            scene.Add(new Circle(scale * (45 + 90 * h), scale * 80, scale * 5.5, 4));
-            scene.Add(new Circle(scale * (315 - 90 * h), scale * 80, scale * 5.5, 5));
-            scene.Add(new Circle(scale * 180, scale * 80, scale * 5.5, 6));
+            scene.Add(new Circle(scale * 180, scale * 80, scale * 5.5, 2));
+            scene.Add(new Circle(scale * (30 + 120 * h), scale * 80, scale * 5.5, 3));
+            scene.Add(new Circle(scale * 180, scale * (140 - 40 * h), scale * 5.5, 4));
+            scene.Add(new Circle(scale * (330 - 120 * h), scale * 80, scale * 5.5, 5));
+            scene.Add(new Circle(scale * 180, scale * (20 + 40 * h), scale * 5.5, 6));
             return scene;
         }
 
         private void CreateButton_Click(object sender, EventArgs e)
         {
-            var step = 5;
+            var step = 50;
 
             var lines = new List<string>();
             for (var i = 0; i < 300; i += step)
@@ -47,15 +53,15 @@ namespace Example1
                 var layer = CreateLayer(i);
                 var bitmap = layer.ToBitmap();
                 var slice = 1 + i / step;
-                Directory.CreateDirectory("slicesTest");
-                bitmap.Save($"slicesTest/slice{slice}.png");
+                Directory.CreateDirectory("slices");
+                bitmap.Save($"slices/slice{slice}.png");
                 DrawBitmap(bitmap);
                 lines.Add(layer.ToText(slice, false));
-                progressBar1.Value = (int)(100.0 * slice / (1 + 300.0 / step));
+                progressBar1.Value = (int)(100.0 * slice / (300.0 / step));
                 Refresh();
             }
 
-            File.WriteAllLines("outputTest.txt", lines);
+            File.WriteAllLines("output.txt", lines);
         }
 
 
