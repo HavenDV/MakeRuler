e35fe1a55d55378f5aa27ba76cc7cc95ed6a5743
Konstantin S. <havendv@gmail.com>
2016-12-26 17:25:09 +1000

    Extracted Extensions

---
+++ MakeRuler/Extensions/DoubleExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MakeRuler.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static int Round(this double value)
+        {
+            //return Convert.ToInt32(value);
+            return (int)Math.Round(value, MidpointRounding.AwayFromZero);
+        }
+
+        public static int RoundMax(this double value)
+        {
+            return (int)Math.Round(value, MidpointRounding.AwayFromZero);
+        }
+
+        public static int RoundMin(this double value)
+        {
+            var smallest = Math.Floor(value);
+            var delta = value - smallest;
+            if (delta <= 0.5)
+            {
+                return (int)smallest;
+            }
+            return (int)(smallest + 1.0);
+        }
+    }
+}
---
+++ MakeRuler/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MakeRuler.Extensions
+{
+    public static class StringExtensions
+    {
+        public static List<string> ToLines(this string text)
+        {
+            return Regex.Split(text, "\r\n|\r|\n").ToList();
+        }
+
+        public static List<string> ToWords(this string text)
+        {
+            return text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
