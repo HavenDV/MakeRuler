bd88e15cc5d13784425e073cf0f695119b49ab94
Konstantin S. <havendv@gmail.com>
2016-12-26 16:07:38 +1000

    Added all tests for Conventer.FromText

--- MakeRuler.Test/Converter.Test.cs
+++ MakeRuler.Test/Converter.Test.cs
@@ -8,6 +8,48 @@ namespace MakeRuler.Extensions.Test
     [TestClass]
     public class Converter_Test
     {
+        public void AreEqualRows(KeyValuePair<int, Row> expected, KeyValuePair<int, Row> actual)
+        {
+            Assert.AreEqual(expected.Key, actual.Key);
+            Assert.AreEqual(expected.Value.Data.Count, actual.Value.Data.Count);
+
+            var expectedLines = expected.Value.ToLines();
+            var actualLines = actual.Value.ToLines();
+            Assert.AreEqual(expectedLines.Count, actualLines.Count);
+            for (var i = 0; i < actualLines.Count; ++i)
+            {
+                Assert.AreEqual(expectedLines[i].Start, actualLines[i].Start);
+                Assert.AreEqual(expectedLines[i].End, actualLines[i].End);
+                Assert.AreEqual(expectedLines[i].Material, actualLines[i].Material);
+            }
+        }
+
+        public void AreEqualLayers(KeyValuePair<int, Scene> expected, KeyValuePair<int, Scene> actual)
+        {
+            Assert.AreEqual(expected.Key, actual.Key);
+
+            var expectedRows = expected.Value.Rows.ToList();
+            var actualRows = actual.Value.Rows.ToList();
+            Assert.AreEqual(expectedRows.Count, actualRows.Count);
+
+            for (var i = 0; i < actualRows.Count; ++i)
+            {
+                AreEqualRows(expectedRows[i], actualRows[i]);
+            }
+        }
+
+        public void AreEqualScene(SortedDictionary<int, Scene> expected, SortedDictionary<int, Scene> actual)
+        {
+            var expectedLayers = expected.ToList();
+            var actualLayers = actual.ToList();
+            Assert.AreEqual(expectedLayers.Count, actualLayers.Count);
+
+            for (var i = 0; i < actualLayers.Count; ++i)
+            {
+                AreEqualLayers(expectedLayers[i], actualLayers[i]);
+            }
+        }
+
         [TestMethod]
         public void Converter_ToText_SimpleRow()
         {
@@ -164,22 +206,6 @@ ROW NR.   2  FIRST PIXEL:   2  NUMBER OF AREAS:  1
             Assert.AreEqual(expected, actual);
         }
 
-        public void AreEqualRows(KeyValuePair<int, Row> expected, KeyValuePair<int, Row> actual)
-        {
-            Assert.AreEqual(expected.Key, actual.Key);
-            Assert.AreEqual(expected.Value.Data.Count, actual.Value.Data.Count);
-            
-            var expectedLines = expected.Value.ToLines();
-            var actualLines = actual.Value.ToLines();
-            Assert.AreEqual(expectedLines.Count, actualLines.Count);
-            for (var i = 0; i < actualLines.Count; ++i)
-            {
-                Assert.AreEqual(expectedLines[i].Start, actualLines[i].Start);
-                Assert.AreEqual(expectedLines[i].End, actualLines[i].End);
-                Assert.AreEqual(expectedLines[i].Material, actualLines[i].Material);
-            }
-        }
-
         [TestMethod]
         public void Converter_FromText_SimpleRow()
         {
@@ -207,32 +233,6 @@ ROW NR.   2  FIRST PIXEL:   2  NUMBER OF AREAS:  1
             AreEqualRows(expected, actual);
         }
 
-        public void AreEqualLayers(KeyValuePair<int, Scene> expected, KeyValuePair<int, Scene> actual)
-        {
-            Assert.AreEqual(expected.Key, actual.Key);
-            
-            var expectedRows = expected.Value.Rows.ToList();
-            var actualRows = actual.Value.Rows.ToList();
-            Assert.AreEqual(expectedRows.Count, actualRows.Count);
-
-            for (var i = 0; i < actualRows.Count; ++i)
-            {
-                AreEqualRows(expectedRows[i], actualRows[i]);
-            }
-        }
-
-        public void AreEqualScene(SortedDictionary<int, Scene> expected, SortedDictionary<int, Scene> actual)
-        {
-            var expectedLayers = expected.ToList();
-            var actualLayers = actual.ToList();
-            Assert.AreEqual(expectedLayers.Count, actualLayers.Count);
-
-            for (var i = 0; i < actualLayers.Count; ++i)
-            {
-                AreEqualLayers(expectedLayers[i], actualLayers[i]);
-            }
-        }
-
         [TestMethod]
         public void Converter_FromText_SimpleIntegerRect()
         {
@@ -253,114 +253,126 @@ ROW NR.   2  FIRST PIXEL:   1  NUMBER OF AREAS:  1
 
             AreEqualScene(expected, actual);
         }
-        /*
+        
         [TestMethod]
-        public void Converter_ToText_SimpleDecimalRect()
+        public void Converter_FromText_SimpleDecimalRect()
         {
             #region TestData
-            var scene = new Scene();
-            scene.Add(new Rect(0.5, 0.5, 1.5, 1.5, 2));
-            #endregion
-
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  2
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  2
 ROW NR.   1  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     2   2
 ROW NR.   2  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     2   2";
-            var actual = scene.ToText(1, false);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Converter_ToText_SimpleDecimalRect2()
-        {
-            #region TestData
-            var scene = new Scene();
-            scene.Add(new Rect(0.8, 0.8, 1.8, 1.8, 3));
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Rect(0.5, 0.5, 1.5, 1.5, 2));
+            expected.Add(1, layer);
             #endregion
 
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  2  LAST ROW:  2
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Converter_FromText_SimpleDecimalRect2()
+        {
+            #region TestData
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  2  LAST ROW:  2
 ROW NR.   2  FIRST PIXEL:   2  NUMBER OF AREAS:  1
     2   3";
-            var actual = scene.ToText(1, false);
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Rect(0.8, 0.8, 1.8, 1.8, 3));
+            expected.Add(1, layer);
+            #endregion
 
-            Assert.AreEqual(expected, actual);
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
         }
 
         [TestMethod]
-        public void Converter_ToText_SimpleIntegerCircle()
+        public void Converter_FromText_SimpleIntegerCircle()
         {
             #region TestData
-            var scene = new Scene();
-            scene.Add(new Circle(1, 1, 1, 4));
-            #endregion
-
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  2
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  2
 ROW NR.   1  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     2   4
 ROW NR.   2  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     2   4";
-            var actual = scene.ToText(1, false);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Converter_ToText_SimpleDecimalCircle()
-        {
-            #region TestData
-            var scene = new Scene();
-            scene.Add(new Circle(1.5, 1.5, 1, 5));
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Circle(1, 1, 1, 4));
+            expected.Add(1, layer);
             #endregion
 
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  3
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Converter_FromText_SimpleDecimalCircle()
+        {
+            #region TestData
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  3
 ROW NR.   1  FIRST PIXEL:   2  NUMBER OF AREAS:  1
     2   5
 ROW NR.   2  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     3   5
 ROW NR.   3  FIRST PIXEL:   2  NUMBER OF AREAS:  1
     2   5";
-            var actual = scene.ToText(1, false);
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Circle(1.5, 1.5, 1, 5));
+            expected.Add(1, layer);
+            #endregion
 
-            Assert.AreEqual(expected, actual);
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
         }
 
         [TestMethod]
 
-        public void Converter_ToText_SimpleDecimalCircle2()
+        public void Converter_FromText_SimpleDecimalCircle2()
         {
             #region TestData
-            var scene = new Scene();
-            scene.Add(new Circle(1.5, 1.5, 1.5, 6));
-            #endregion
-
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  3
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  1  LAST ROW:  3
 ROW NR.   1  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     3   6
 ROW NR.   2  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     3   6
 ROW NR.   3  FIRST PIXEL:   1  NUMBER OF AREAS:  1
     3   6";
-            var actual = scene.ToText(1, false);
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Circle(1.5, 1.5, 1.5, 6));
+            expected.Add(1, layer);
+            #endregion
 
-            Assert.AreEqual(expected, actual);
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
         }
 
         [TestMethod]
-        public void Converter_ToText_SimpleDecimalCircle3()
+        public void Converter_FromText_SimpleDecimalCircle3()
         {
             #region TestData
-            var scene = new Scene();
-            scene.Add(new Circle(1.5, 1.5, 0.5, 8));
-            #endregion
-
-            var expected = @"SLICE NUMBER:  1  FIRST ROW:  2  LAST ROW:  2
+            var text = @"SLICE NUMBER:  1  FIRST ROW:  2  LAST ROW:  2
 ROW NR.   2  FIRST PIXEL:   2  NUMBER OF AREAS:  1
     2   8";
-            var actual = scene.ToText(1, false);
+            var expected = new SortedDictionary<int, Scene>();
+            var layer = new Scene();
+            layer.Add(new Circle(1.5, 1.5, 0.5, 8));
+            expected.Add(1, layer);
+            #endregion
 
-            Assert.AreEqual(expected, actual);
-        }*/
+            var actual = Conventer.SceneFromText(text);
+
+            AreEqualScene(expected, actual);
+        }
     }
 }
