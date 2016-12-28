0a1b5c1a4b203ca08f28ead70f4749d9b86cd2b8
Konstantin S. <havendv@gmail.com>
2016-12-28 15:24:51 +1000

    Changed Form

--- MakeRuler/Scene.cs
+++ MakeRuler/Scene.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,37 @@ namespace MakeRuler
 {
     public class Scene
     {
-        public SortedDictionary<int, Slice> Slices { get; set; }
+        public SortedDictionary<int, Slice> Slices { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Depth { get; set; }
 
         public Scene()
         {
             Slices = new SortedDictionary<int, Slice>();
+            Width = 1;
+            Height = 1;
+            Depth = 300;
+        }
+
+        public void AddSlice(int sliceId, Slice slice)
+        {
+            Slices.Add(sliceId, slice);
+            Width = Math.Max(Width, slice.Width);
+            Height = Math.Max(Height, slice.Height);
+        }
+
+        public Bitmap ToFrontBitmap()
+        {
+            var bitmap = new Bitmap(Width, Depth);
+            var g = Graphics.FromImage(bitmap);
+            foreach (var slice in Slices)
+            {
+                var centerRow = slice.Value.CenterRow;
+                g.DrawImage(Slice.RowToBitmap(centerRow), new Point(0, (int)(Depth - Depth * (slice.Key-0.5) / Slices.Count)));
+            }
+
+            return bitmap;
         }
     }
 }
